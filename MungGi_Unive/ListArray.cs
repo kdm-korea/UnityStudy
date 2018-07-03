fa5a00b72462688e212e69b1c93c97ceaab73cac
kdm-korea <kdm_korea@naver.com>
2018-07-03 20:12:29 +0900

    Test: Study Unity

---
+++ MungGi_Unive/ListArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Product {
+    public stirng name;
+    public int price;
+}
+
+public class Class1
+{
+	static void Main(stinrg[] args) {
+
+        List<Product> PList = new List<Product>() 
+        {
+        new Product() {name="jeyuk", price = 7000 },
+        new Product() {name="yukgae", price = 8000},
+        new Product() {name="gimchi", price = 6000}
+        };//{array list}
+
+        PList.Add(new Product() {name = "karanfri", price = 1000});
+        //PList.Remove();
+
+        //PList.Sort((a,b) => a.CompareTo(b));
+        PList.Sort((a, b) => a.name.CompareTo(b).name);
+
+        foreach (var item in PList) {
+            Console.WriteLine(item.name + " : " + item.price);
+        }
+    }
+}
