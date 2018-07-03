dd4d2c669aa4f4ba66d628c21d7d2fcb03aec7ad
kdm-korea <kdm_korea@naver.com>
2018-07-03 20:12:01 +0900

    Test: Study Unity

---
+++ MungGi_Unive/Nuclear.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace app {
+    class Nuclear {
+        private bool fire;
+
+        public int Fire { //속성 ()X [read only]
+            get { return fire; } 
+            //set { fire = value; }
+        }
+
+        public Nuclear() {
+            fire = false;
+        }
+
+        public void ChkPW(string passwd) {
+            if (passwd == "binggo") {
+                fire = true;
+            }
+        }
+        public void Lanch() {
+            if (fire == true) {
+                Console.WriteLine("Fire!!");
+            } else {
+                Console.WriteLine("Access Denied");
+            }
+        }
+    }
+
+    public class MainApp {
+        static void Main(string[] args) {
+            Nuclear nc = new Nuclear();
+
+            nc.Chkpw("III");
+            nc.Lanch();
+
+            while(nc.Fire == true) {
+                Console.WriteLine(nc.file());
+            }
+        }
+    }
+}
