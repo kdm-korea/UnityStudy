97c22c4a2aacdedc42023a24da1bfb9af3a61923
kdm-korea <kdm_korea@naver.com>
2018-07-05 00:12:42 +0900

    Test: Study Unity

---
+++ SpaceShooter/Assets/02. Scripts/FireCtrl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireCtrl : MonoBehaviour {
+    //Bullet Prefabs
+    public GameObject bullet;
+    //Bullet fire Location
+    public Transform firePos;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        //Moues leftButtonClick
+        if (Input.GetMouseButtonDown(0))
+        {
+            Fire();
+        }
+	}
+
+    void Fire()
+    {
+        //Bullet Prefabs Dynamic produce
+        Instantiate(bullet, firePos.position, firePos.rotation);
+    }
+}
---
+++ SpaceShooter/Assets/02. Scripts/RemoveBullet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveBullet : MonoBehaviour {
+    //충돌 시 발생 이벤트
+    private void OnCollisionEnter(Collision coll)
+    {
+        //충돌한 게임오브젝트의 태그값 비교
+        if(coll.collider.tag == "BULLET")
+        {
+            //충돌한 게임오브젝트 삭제
+            Destroy(coll.gameObject);
+        }
+    }
+}
